c11eddbf172ed0359396f5bc6d336106fa35a939
devkn <kimoueric04@gmail.com>
2023-06-26 22:29:10 +0000

    programme CRUD

---
+++ assets/index.js
@@ -0,0 +1,107 @@
+// Insertion de donnees dans la base de donnees
+
+const idInput = document.querySelector("#id_user");
+const nameInput = document.querySelector("#nom");
+const lastnameInput = document.querySelector("#prenoms");
+const jobInput = document.querySelector("#profession");
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let data = {
+    id_user: idInput.value,
+    nom: nameInput.value,
+    prenoms: lastnameInput.value,
+    profession: jobInput.value,
+  };
+
+  if (document.querySelector("#btnAddOrChange").textContent === "Soumettre") {
+    Utilisateurs.insertUser(data, function (erreur) {
+      alert(erreur);
+    });
+
+    idInput.value = "";
+    nameInput.value = "";
+    lastnameInput.value = "";
+    jobInput.value = "";
+
+    document.querySelector("table tbody").innerHTML = "";
+    showData();
+  } else {
+    Utilisateurs.updateUser(data, function () {
+      idInput.value = "";
+      nameInput.value = "";
+      lastnameInput.value = "";
+      jobInput.value = "";
+      if (document.querySelector("#btnAddOrChange").textContent === "Modifier")
+        document.querySelector("#btnAddOrChange").textContent = "Soumettre";
+      document.querySelector("table tbody").innerHTML = "";
+      document.querySelector("#id_user").removeAttribute("readonly");
+      showData();
+    });
+  }
+});
+
+// // Affichage des donnees sur le DOM
+
+function showData() {
+  let recupData = Utilisateurs.allData();
+
+  let body = document.querySelector("table tbody");
+
+  recupData.forEach((utilisateur) => {
+    let deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("btn");
+    deleteBtn.classList.add("btn-danger");
+    deleteBtn.textContent = "Supprimer";
+
+    deleteBtn.addEventListener("click", (e) => {
+      Utilisateurs.deleteUser(utilisateur.id_user, function (erreur) {
+        console.log(erreur);
+      });
+      document.querySelector("table tbody").innerHTML = "";
+      showData();
+    });
+
+    let upadteBtn = document.createElement("button");
+    upadteBtn.classList.add("btn");
+    upadteBtn.classList.add("btn-primary");
+    upadteBtn.textContent = "Modifier";
+
+    upadteBtn.addEventListener("click", (e) => {
+      Utilisateurs.oneUser(utilisateur.id_user, function (dataUser) {
+        document.querySelector("#id_user").setAttribute("readonly", true);
+        idInput.value = dataUser.id_user;
+        nameInput.value = dataUser.nom;
+        lastnameInput.value = dataUser.prenoms;
+        jobInput.value = dataUser.profession;
+        if (
+          document.querySelector("#btnAddOrChange").textContent === "Soumettre"
+        )
+          document.querySelector("#btnAddOrChange").textContent = "Modifier";
+      });
+    });
+
+    let ligne = body.insertRow();
+
+    let cellule1 = ligne.insertCell();
+    cellule1.textContent = utilisateur.id_user;
+
+    let cellule2 = ligne.insertCell();
+    cellule2.textContent = utilisateur.nom;
+
+    let cellule3 = ligne.insertCell();
+    cellule3.textContent = utilisateur.prenoms;
+
+    let cellule4 = ligne.insertCell();
+    cellule4.textContent = utilisateur.profession;
+
+    let cellule5 = ligne.insertCell();
+    cellule5.appendChild(upadteBtn);
+
+    let cellule6 = ligne.insertCell();
+    cellule6.appendChild(deleteBtn);
+  });
+}
+
+showData();
---
+++ models/Utilsateurs.js
@@ -0,0 +1,64 @@
+class Utilisateurs {
+  /**
+   *
+   * @param {object} objet
+   * @param {void} cb
+   */
+  static insertUser(objet, cb) {
+    if (!localStorage.getItem(objet.id_user)) {
+      localStorage.setItem(objet.id_user, JSON.stringify(objet));
+    } else {
+      let erreur = "Desole il y'a un utilisateur enregistre avec cet id";
+      cb(erreur);
+    }
+  }
+  /**
+   *
+   * @param {string} id
+   * @param {void} cb
+   */
+
+  static deleteUser(id, cb) {
+    if (localStorage.getItem(id)) localStorage.removeItem(id);
+    else {
+      let erreur = "Desole cet utilisateur n'existe pas !!!";
+      cb(erreur);
+    }
+  }
+/**
+ * 
+ * @param {object} objet 
+ * @param {void} cb 
+ */
+  static updateUser(objet, cb) {
+    
+    const { id_user} = objet
+   
+    if (localStorage.getItem(id_user)) {
+      localStorage.setItem(id_user, JSON.stringify(objet));
+    }
+    cb()
+  
+  }
+
+  static allData() {
+    let dataTable = [];
+    for (let index = 1; index <= localStorage.length; index++) {
+      dataTable.push(JSON.parse(localStorage.getItem(index)));
+    }
+    return dataTable;
+  }
+
+  /**
+   * 
+   * @param {string} id 
+   * @param {void} cb 
+   */
+  static oneUser(id, cb) {
+    let dataUser;
+    if (localStorage.getItem(id))
+      dataUser = JSON.parse(localStorage.getItem(id));
+   
+    cb(dataUser);
+  }
+}
